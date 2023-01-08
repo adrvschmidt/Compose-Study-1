ada8d2dc0bdecabafa505e8d06fb818ece39158c
Adriano Schmidt <adrvschmidt@gmail.com>
2023-01-07 22:17:08 -0300

    Added style and click to text

--- app/src/main/java/br/com/schmidt/estudocompose1/MainActivity.kt
+++ app/src/main/java/br/com/schmidt/estudocompose1/MainActivity.kt
@@ -3,41 +3,43 @@ package br.com.schmidt.estudocompose1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.schmidt.estudocompose1.ui.theme.EstudoCompose1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EstudoCompose1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GreetingText("Android")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingText(name: String) {
+    Text(text = "Hello $name!",
+        modifier = Modifier.size(width = 200.dp, height = 240.dp)
+            .clickable(onClick = {
+
+        }).padding(all = 24.dp),
+        style = MaterialTheme.typography.h5,
+        fontWeight = FontWeight.SemiBold)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EstudoCompose1Theme {
-        Greeting("Android")
+        GreetingText("Android")
     }
 }
