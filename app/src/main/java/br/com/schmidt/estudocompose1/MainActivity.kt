0a35674f84827bcdc806a0c3922a2aad1e4ee2d2
Adriano Schmidt <adrvschmidt@gmail.com>
2023-01-12 19:35:50 -0300

    Added mutable state to dynamic list

--- app/src/main/java/br/com/schmidt/estudocompose1/MainActivity.kt
+++ app/src/main/java/br/com/schmidt/estudocompose1/MainActivity.kt
@@ -3,41 +3,67 @@ package br.com.schmidt.estudocompose1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.schmidt.estudocompose1.ui.theme.EstudoCompose1Theme
 
-val nameList = listOf("John", "Michael", "Andrew", "Danna")
+val nameList = arrayListOf("John", "Michael", "Andrew", "Danna")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen(names = nameList)
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun MainScreen(names: List<String>) {
-    Column {
-        names.forEach { name ->
-            Greeting(name = name)
+fun MainScreen() {
+    val greetingListState = remember {
+        mutableStateListOf("John")
+    }
+
+    Column(modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.SpaceEvenly,
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        GreetingList(greetingListState) {
+            greetingListState.add("New Name")
         }
+    }
+}
+
+@Composable
+fun GreetingList(greetingListState: SnapshotStateList<String>, buttonClick: () -> Unit) {
+    greetingListState.forEachIndexed { index, name ->
+        Greeting(name = "$name $index")
+    }
+    Button(onClick = buttonClick) {
+        Text(text = "Add New Name")
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+    style = MaterialTheme.typography.h5)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EstudoCompose1Theme {
-        MainScreen(names = nameList)
+        MainScreen()
     }
 }
