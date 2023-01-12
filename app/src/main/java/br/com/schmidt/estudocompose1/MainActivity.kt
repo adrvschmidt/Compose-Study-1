bd0ad42a460b9cda52cb7d4affb6ea18d240dbed
Adriano Schmidt <adrvschmidt@gmail.com>
2023-01-12 19:02:59 -0300

    Added column and text with a list of names

--- app/src/main/java/br/com/schmidt/estudocompose1/MainActivity.kt
+++ app/src/main/java/br/com/schmidt/estudocompose1/MainActivity.kt
@@ -3,43 +3,41 @@ package br.com.schmidt.estudocompose1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import br.com.schmidt.estudocompose1.ui.theme.EstudoCompose1Theme
+
+val nameList = listOf("John", "Michael", "Andrew", "Danna")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingText("Android")
+            MainScreen(names = nameList)
         }
     }
 }
 
 @Composable
-fun GreetingText(name: String) {
-    Text(text = "Hello $name!",
-        modifier = Modifier.size(width = 200.dp, height = 240.dp)
-            .clickable(onClick = {
+fun MainScreen(names: List<String>) {
+    Column {
+        names.forEach { name ->
+            Greeting(name = name)
+        }
+    }
+}
 
-        }).padding(all = 24.dp),
-        style = MaterialTheme.typography.h5,
-        fontWeight = FontWeight.SemiBold)
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EstudoCompose1Theme {
-        GreetingText("Android")
+        MainScreen(names = nameList)
     }
 }
